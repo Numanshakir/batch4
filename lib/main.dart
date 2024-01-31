17553a7f5eb7f6797d4a0ff12b451e900e6adebe
Numan Shakir <numanshakir248@gmail.com>
2024-01-31 15:30:27 +0500

    fcm

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,58 @@ import 'package:batch4/Localization/demo_splash.dart';
 import 'package:batch4/Localization/demo_view.dart';
 import 'package:batch4/Localization/translation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  print("FCM ${await messaging.getToken()}");
+// The following line will enable the Android and iOS wakelock.
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    debugPrint('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    debugPrint('User granted provisional permission');
+  } else {
+    debugPrint('User declined or has not accepted permission');
+  }
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((event) {
+    print("onMessage");
+    print(event.notification!.title);
+    print(event.notification!.body);
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print("onMessageOpenedApp");
+    print(event.notification!.title);
+    print(event.notification!.body);
+  });
   runApp(const MyApp());
 }
 
